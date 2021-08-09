b7c6e01c78d906d69fe83496d132fb020e51014b
Jatinder Singh <jatinder303@gmail.com>
2021-08-09 16:44:48 +1200

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Widget;
+
+namespace AutoComplete_Xamarin_Android
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        AutoCompleteTextView acweekdays;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+
+            var weekday_array = Resources.GetStringArray(Resource.Array.weekdays_array);
+
+            //var weekday_array = new string[] { "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday" , "Sunday","Wednesday-2","Sunday-2"};
+            acweekdays = FindViewById<AutoCompleteTextView>(Resource.Id.actxtWeekdays);
+            acweekdays.Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.TestListItem, weekday_array);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
